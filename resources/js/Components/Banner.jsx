b09ca7f6631dc171e9509ea6cbada3f0af5abe33
Jasson López <jasson_alex99@hotmail.com>
2022-11-19 17:48:26 -0600

    :sparkles: img width

--- resources/js/Components/Banner.jsx
+++ resources/js/Components/Banner.jsx
@@ -2,7 +2,7 @@ const Banner = ({ banner_img }) => {
     return (
         <div className="w-full">
             <div className="w-full h-[150px] lg:h-[450px] overflow-hidden">
-                <img src={`/storage/${banner_img}`} />
+                <img src={`/storage/${banner_img}`} className="w-full" />
             </div>
         </div>
     );
