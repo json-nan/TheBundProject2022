ac4129cbde52861548c191730fa7399f0401b936
Jasson López <jasson_alex99@hotmail.com>
2022-11-19 17:51:53 -0600

    :sparkles: img resize

--- resources/js/Components/Banner.jsx
+++ resources/js/Components/Banner.jsx
@@ -1,8 +1,8 @@
 const Banner = ({ banner_img }) => {
     return (
         <div className="w-full">
-            <div className="w-full h-[150px] lg:h-[450px] overflow-hidden">
-                <img src={`/storage/${banner_img}`} className="w-full" />
+            <div className="w-full h-[150px] lg:h-[400px] overflow-hidden">
+                <img src={`/storage/${banner_img}`} className="h-full" />
             </div>
         </div>
     );
