68a3ef0e5cd45338270982637a8b6d2d578e54d2
Jasson López <jasson_alex99@hotmail.com>
2022-11-20 13:33:38 -0600

    :sparkles: Building

--- public/build/assets/InputLabel.7848c16e.js
+++
@@ -1,1 +0,0 @@
-import{a as m}from"./app.f0d7b5ae.js";function l({forInput:e,value:t,className:r,children:a}){return m("label",{htmlFor:e,className:"block font-medium text-sm text-gray-700 "+r,children:t||a})}export{l as I};
