253957ef497ea86ffc0a07877140ea7996b78b14
Jasson López <30847798+jewj999@users.noreply.github.com>
2022-11-20 10:58:46 -0600

    :sparkles: Fix Banner

--- resources/js/Components/Banner.jsx
+++ resources/js/Components/Banner.jsx
@@ -2,7 +2,7 @@ const Banner = ({ banner_img }) => {
     return (
         <div className="w-full">
             <div className="w-full h-[150px] lg:h-[400px] overflow-hidden">
-                <img src={`/storage/${banner_img}`} className="h-full" />
+                <img src={`/storage/${banner_img}`} className="w-full -mt-6" />
             </div>
         </div>
     );
